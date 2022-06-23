91d8264116ece0b9d18fc5cf7c815a2beb4b52e0
LuhanVini <luhanvini.dev@gmail.com>
2022-06-23 20:19:46 -0300

    UP
    NOVO UPDATE

---
+++ login.php
@@ -0,0 +1,132 @@
+<?php
+// Inicialize a sessão
+session_start();
+ 
+// Verifique se o usuário já está logado, em caso afirmativo, redirecione-o para a página de boas-vindas
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Incluir arquivo de configuração
+require_once "config.php";
+ 
+// Defina variáveis e inicialize com valores vazios
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processando dados do formulário quando o formulário é enviado
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Verifique se o nome de usuário está vazio
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor, insira o nome de usuário.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Verifique se a senha está vazia
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor, insira sua senha.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validar credenciais
+    if(empty($username_err) && empty($password_err)){
+        // Prepare uma declaração selecionada
+        $sql = "SELECT id, username, password FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+            // Vincule as variáveis à instrução preparada como parâmetros
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            // Definir parâmetros
+            $param_username = trim($_POST["username"]);
+            
+            // Tente executar a declaração preparada
+            if($stmt->execute()){
+                // Verifique se o nome de usuário existe, se sim, verifique a senha
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $id = $row["id"];
+                        $username = $row["username"];
+                        $hashed_password = $row["password"];
+                        if(password_verify($password, $hashed_password)){
+                            // A senha está correta, então inicie uma nova sessão
+                            session_start();
+                            
+                            // Armazene dados em variáveis de sessão
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirecionar o usuário para a página de boas-vindas
+                            header("location: welcome.php");
+                        } else{
+                            // A senha não é válida, exibe uma mensagem de erro genérica
+                            $login_err = "Nome de usuário ou senha inválidos.";
+                        }
+                    }
+                } else{
+                    // O nome de usuário não existe, exibe uma mensagem de erro genérica
+                    $login_err = "Nome de usuário ou senha inválidos.";
+                }
+            } else{
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+
+            // Fechar declaração
+            unset($stmt);
+        }
+    }
+    
+    // Fechar conexão
+    unset($pdo);
+}
+?>
+ 
+ <!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Painel de Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Links --->
+    <script src="https://kit.fontawesome.com/6e587148a4.js" crossorigin="anonymous" defer></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="icon" href="">
+</head>
+<body>
+      
+    <header>
+        <h1>Projeto_ERP</h1>
+    </header>
+
+    
+    <main>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="conteiner">
+                <label >Nickname</label> <br>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>" id="login" placeholder="Digite seu login...">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>
+            <div class="conteiner">
+                <label >Senha</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" id="senha" placeholder="Digite sua senha aqui..." >
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Entrar" id="entrar">
+            </div>
+            <p>Não tem uma conta? <a href="register.php">Inscreva-se agora</a>.</p>
+        </form>
+    </main>
+</body>
+</html>
